6883accc0a8c86dc8548f4a9a2d5770f01cb8942
hmman3141 <huaminhman3141@gmail.com>
2022-02-22 10:45:28 +0700

    main, add new project

---
+++ netcore-redux-id4-rookie4/src/Rookie.Ecom.Customer/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Rookie.Ecom.Business.Interfaces;
+using Rookie.Ecom.Contracts.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rookie.Ecom.Customer.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ICategoryService _categoryService;
+
+        public IndexModel(ILogger<IndexModel> logger, ICategoryService categoryService)
+        {
+            _logger = logger;
+            _categoryService = categoryService;
+        }
+
+        public IEnumerable<CategoryDto> GetAsync => _categoryService.GetAllAsync().Result;
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
