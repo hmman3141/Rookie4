dc142cb03d98c2d909c5e8bd80d0240d930ae469
hmman3141 <huaminhman3141@gmail.com>
2022-03-07 00:39:13 +0700

    main, Customer_FE_Add_Cart_Update_OnGet_ProductDetail

--- netcore-redux-id4-rookie4/src/Rookie.Ecom.Customer/Pages/ProductDetail.cshtml.cs
+++ netcore-redux-id4-rookie4/src/Rookie.Ecom.Customer/Pages/ProductDetail.cshtml.cs
@@ -4,15 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Rookie.Ecom.Business.Interfaces;
+using Rookie.Ecom.Contracts.Dtos;
 
 namespace Rookie.Ecom.Customer.Pages
 {
     public class ProductDetailModel : PageModel
     {
         public String name = "";
+        private readonly ICategoryService categoryService;
+        public ProductDetailModel(ICategoryService _categoryService)
+        {
+            categoryService = _categoryService;
+        }
+
+        public IEnumerable<CategoryDto> categoryDtos;
+
         public void OnGet(String product)
         {
             name = product;
+            categoryDtos = categoryService.GetAllByAsync(x => 1 == 1).Result;
         }
     }
 }
